e9a3fc4dc2b6dff0a74d6c1acb10590450d523d0
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-03-26 21:07:25 +0700

    save

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    domains: ["encrypted-tbn0.gstatic.com", "upload-os-bbs.hoyolab.com"]
+  },
   reactCompiler: true,
 };
 
